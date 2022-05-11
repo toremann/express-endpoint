864fe8e6166263a592fdc2b79deced1034928feb
toremann <50668920+toremann@users.noreply.github.com>
2022-05-11 23:03:02 +0200

    added params

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const morgan = require("morgan");
-const fs = require("fs");
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
 
-const path = require("path");
+const path = require('path');
 const app = express();
-var ip = require("ip");
+var ip = require('ip');
 app.use(morgan('short'));
 
-const port = "3000"
+const port = '3001'
 
 app.get('/', (req, res) => {
     res.status(200).send('API ok');
@@ -15,16 +15,29 @@ app.get('/', (req, res) => {
 
 // Endpoint for data ip:port/data/
 
-app.get("/data/", (req, res) => {
-    return fs.readFile(path.join(__dirname, "/data/data.json"), (err, data) => {
+app.get('/data/', (req, res) => {
+    return fs.readFile(path.join(__dirname, '/data/data.json'), (err, data) => {
       if (err) {
-        res.status(500).send("Stock API down");
+        res.status(500).send('Stock API down');
       }
   
       res.status(200).send(JSON.parse(data.toString()));
     });
   });
-    
+
+  app.param('stock', function(req, res, next, stock) {
+    const modified = stock.toUpperCase();
+  
+    req.stock = modified;
+    next();
+  });
+  
+  app.get('/data/:stock', (req, res) => {
+    return fs.readFile(path.join(__dirname, '/data/'+`${req.stock}`+'.json'), (err, data) => {
+    res.status(200).send(JSON.parse(data.toString()));
+  });
+});
+  
   app.listen(port, () => {
-    console.log("Server started", ip.address(), port);
+    console.log('Server started', ip.address(), port);
   });
